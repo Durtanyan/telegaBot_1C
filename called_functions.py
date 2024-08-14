23dcd10d9a51d06f81500891e3b21b7273d8a4f5
LukinSV <lukins.ya@yandex.ru>
2024-08-14 12:45:02 +0300

    Работают кнопки start, help, command1, добавление пользователя и подписка на отчеты

--- called_functions.py
+++ called_functions.py
@@ -1,6 +1,32 @@
 from requests import get
+import shelve as sh
+
+
+filename = "db_users"
+
 
 def get_image_cat():
-	url_request = 'https://api.thecatapi.com/v1/images/search'
-	answer = get(url_request).json()
-	return answer[0]["url"]
+    url_request = 'https://api.thecatapi.com/v1/images/search'
+    answer = get(url_request).json()
+    return answer[0]["url"]
+
+
+def add_user_in_db(user_name, chat_id):
+    f_db = sh.open(filename)
+    if user_name in f_db.keys():
+        return False
+    f_db[user_name] = [chat_id, []]
+    f_db.close()
+    return True
+
+
+def add_reports_to_subscription(user_name, list_reports):
+    f_db = sh.open(filename)
+    if user_name in f_db.keys():
+        list_data = f_db[user_name]
+        # Каждый раз перезаполняем список отчетов по подписке заново.
+        copy_list = list()
+        [copy_list.append(i) for i in list_reports]
+        list_data[1] = copy_list
+        f_db[user_name] = list_data
+    f_db.close()
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+from aiogram.filters import Command  # type: ignore
 from aiogram.types import Message
 import called_functions
 import config
+import text_constants
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 BOT_TOKEN = config.BOT_TOKEN
+TRUE_MESSAGEREGISTRATION = text_constants.TRUE_MESSAGEREGISTRATION
+FALSE_MESSAGEREGISTRATION = text_constants.FALSE_MESSAGEREGISTRATION
+HELP_MESSAGE = text_constants.HELP_MESSAGE
+names_available_reports = text_constants.NAMES_AVAILABLE_REPORTS
+
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
@@ -16,40 +22,69 @@ dp = Dispatcher()
 async def process_start_command(message: Message):
     user_name = message.chat.username
     chat_id = message.chat.id
-    await message.answer(f'Привет, {user_name}!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь.\
-        \nА пока посмотри котиков.')
+    add_user_in_db = called_functions.add_user_in_db(user_name, chat_id)
+    if add_user_in_db:
+        await message.answer(f'Привет, {user_name}! {TRUE_MESSAGEREGISTRATION}\n{HELP_MESSAGE}')
+    else:
+        await message.answer(f'Прошу прощения, {user_name}! {FALSE_MESSAGEREGISTRATION}')
     url = called_functions.get_image_cat()
     await bot.send_photo(chat_id, url)
 
 
 # Этот хэндлер будет срабатывать на команду "/help
 async def process_help_command(message: Message):
-    await message.answer(
-        'Напиши мне что-нибудь и в ответ '
-        'я пришлю тебе твое сообщение'
-    )
-    
+    chat_id = message.chat.id
+    await bot.send_message(chat_id, HELP_MESSAGE)
+
 
 # Этот хэндлер будет срабатывать на команду "/command1"
+# Просто выводит список доступных для подписки отчетов с их номерами
 async def process_command_1(message: Message):
-    await message.answer('Тест. Получены доступные для подписки отчеты.')
-    
+    chat_id = message.chat.id
+    list_available_reports = [str(i + 1) + ". " + names_available_reports[i]
+                              for i in range(len(names_available_reports))]
+    await bot.send_message(chat_id, "\n".join(list_available_reports))
+
 
 # Этот хэндлер будет срабатывать на команду "/command2"
 async def process_command_2(message: Message):
     await message.answer('Тест. Получены отчеты по подписке.')
-    
+
 
 # Этот хэндлер будет срабатывать на команду "/command3"
 async def process_command_3(message: Message):
     await message.answer('Тест. Вы отписались от получения отчетов по подписке.')
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения
+# кроме тех команд, что расположены выше по регистрации
+# Валидные данные это номера доступных для подписки отчетов
+# Все остальное невалидно.
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
-    
+    user_name = message.chat.username
+    chat_id = message.chat.id
+    # Считываем сообщение пользователя из телеги
+    list_data = message.text.split(" ")
+    try:
+        # Преобразуем в список чисел
+        # При ошибке уходим в исключения
+        list_int = list(map(int, list_data))
+        list_int.sort()
+        # Из списка доступных отчетов получает их номера
+        list_index_available_reports = [
+            i + 1 for i in range(len(names_available_reports))]
+        # Все элементы введенные пользователем должны быть в списке номеров доступных к подписке отчетов
+        if all([i in list_index_available_reports for i in list_int]):
+            # Списки равны - идем в БД и заполняем, ищем пользователя и заполняем
+            # второй параметр из списка полученных отчетов. [chat_id, []]
+            called_functions.add_reports_to_subscription(user_name, list_int)
+            await bot.send_message(chat_id, text_constants.SUBSCRIPTION_MESSAGE)
+        else:
+            await bot.send_message(chat_id, text_constants.INVALID_DATA)
+    except:
+        broken_data = text_constants.BROKEN_DATA
+        await bot.send_message(chat_id, broken_data)
+
 
 # Регистрируем хэндлеры
 dp.message.register(process_start_command, Command(commands='start'))
